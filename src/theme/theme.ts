bb13617aa983768ce4e116f8426e28712b7d7673
dorisaward <dorisaward@gmail.com>
2024-12-23 16:50:29 +0000

    Toggle dark mode with button

---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components'
+
+export const lightTheme: DefaultTheme = {
+    backgroundColour: '#CCE6FF',
+    secondaryColour: '#FFCCE6',
+    tertiaryColour: '#E6FFCC',
+    fontColour: 'black'
+}
+
+export const darkTheme: DefaultTheme = {
+    backgroundColour: '#214269',
+    secondaryColour: '#692142',
+    tertiaryColour: '#426921',
+    fontColour: 'white'
+}
