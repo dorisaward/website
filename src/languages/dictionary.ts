1cd54db9f45d85e6ae81f594be029f47230437b9
dorisaward <dorisaward@gmail.com>
2025-01-12 17:52:38 +0000

    Homepage says hello instead of sorry

--- src/languages/dictionary.ts
+++ src/languages/dictionary.ts
@@ -4,12 +4,14 @@ import { Dictionaries } from './types/Dictionaries.ts'
 export const dictionary: Dictionaries = {
     [languages.en]: {
         'sorry': 'Sorry',
+        'hello': 'Hello!',
         'error': 'Nothing to see here',
         'home': 'Home',
         'cv': 'Curriculum Vitae',
     },
     [languages.th]: {
         'sorry': 'ขอโทษ',
+        'hello': 'สวัสดี',
         'error': 'ไม่มีที่นี่',
         'home': 'บ้าน',
         'cv': 'ประวัติย่อ',
