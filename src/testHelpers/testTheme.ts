53440bc99361bda17ab95af92f4e0ab1906d48e7
dorisaward <dorisaward@gmail.com>
2025-02-16 20:35:43 +0000

    Home Page test

---
+++ src/testHelpers/testTheme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+export const testTheme: DefaultTheme = {
+    backgroundColour: 'backgroundColour',
+    fontColour: 'fontColour',
+    padding: 'padding',
+    secondaryColour: 'secondaryColour',
+    shadowColour: 'shadowColour',
+    switchOffColour: 'switchOffColour',
+    switchOnColour: 'switchOnColour',
+    tertiaryColour: 'tertiaryColour'
+}
