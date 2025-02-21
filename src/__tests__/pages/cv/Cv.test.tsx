47423d2dfaa9bb41ee5bc75145be4da97c63ddfe
dorisaward <dorisaward@gmail.com>
2025-02-21 15:16:58 +0000

    update cv test

--- src/__tests__/pages/cv/Cv.test.tsx
+++ src/__tests__/pages/cv/Cv.test.tsx
@@ -4,15 +4,58 @@ import { LangContext } from '../../../languages/types/LanguagesContext.ts'
 import { MockLanguagesProvider } from '../../../testHelpers/MockLanguagesProvider.tsx'
 import { languages } from '../../../languages/languages.ts'
 
-jest.mock('../../../assets/cv.json', () => ({ data: [{ heading: 'cv heading', text: 'cv text' }] }))
-jest.mock('../../../pages/cv/logos/index.ts', () => ({ images: [
-    jest.mock('../../../pages/cv/logos/react.png'),
-    jest.mock('../../../pages/cv/logos/react.png')
-] }))
+jest.mock('../../../assets/cv.json', () => ({
+    'basics': {
+        'name': 'basic name',
+        'label': 'label',
+        'email': 'email',
+        'url': 'url',
+        'phone': 'phone',
+        'location': {
+            'city': 'city',
+            'countryCode': 'country'
+        },
+        'summary': 'basic summary',
+        'profiles': [{
+            'network': 'network',
+            'username': 'username',
+            'url': 'profile url'
+        }]
+    },
+    'work': [{
+        'name': 'work name',
+        'position': 'position',
+        'url': 'work url',
+        'startDate': 'work start',
+        'summary': 'work summary',
+    }],
+    'education': [{
+        'institution': 'institution',
+        'url': 'edu url',
+        'area': 'area',
+        'studyType': 'study',
+        'startDate': 'edu start',
+        'endDate': 'edu end'
+    }],
+    'skills': [{
+        'name': 'skill name',
+        'level': 'level',
+        'keywords': [
+            'keyword'
+        ]
+    }],
+    'languages': [{
+        'language': 'lang',
+        'fluency': 'fluency'
+    }]
+}))
+jest.mock('../../../pages/cv/logos/index.ts', () => ({
+    images: [
+        jest.mock('../../../pages/cv/logos/react.png'),
+        jest.mock('../../../pages/cv/logos/react.png')
+    ]
+}))
 
-const cvHeading = 'cv heading'
-const cvText = 'cv text'
-const pageHeading = 'Curriculum Vitae'
 const thaiLangError = 'Unfortunately, Thai language is not available'
 
 describe('Cv', () => {
@@ -25,14 +68,14 @@ describe('Cv', () => {
         }
 
         // When
-        const { getByText, queryByText, getAllByRole } = render(<MockLanguagesProvider value={value}><Cv/></MockLanguagesProvider>)
+        const { baseElement, queryByText, getAllByRole } = render(
+            <MockLanguagesProvider value={value}><Cv/></MockLanguagesProvider>
+        )
 
         // Then
         expect(queryByText(thaiLangError)).toBeFalsy()
-        expect(getByText(pageHeading)).toBeTruthy()
-        expect(getByText(cvHeading)).toBeTruthy()
-        expect(getByText(cvText)).toBeTruthy()
         expect(getAllByRole('img')).toHaveLength(2)
+        expect(baseElement).toMatchSnapshot()
     })
 
     it('renders, given language thai', () => {
@@ -44,13 +87,13 @@ describe('Cv', () => {
         }
 
         // When
-        const { getByText, getAllByRole } = render(<MockLanguagesProvider value={value}><Cv/></MockLanguagesProvider>)
+        const { getByText, baseElement, getAllByRole } = render(
+            <MockLanguagesProvider value={value}><Cv/></MockLanguagesProvider>
+        )
 
         // Then
         expect(getByText(thaiLangError)).toBeTruthy()
-        expect(getByText(pageHeading)).toBeTruthy()
-        expect(getByText(cvHeading)).toBeTruthy()
-        expect(getByText(cvText)).toBeTruthy()
         expect(getAllByRole('img')).toHaveLength(2)
+        expect(baseElement).toMatchSnapshot()
     })
 })
